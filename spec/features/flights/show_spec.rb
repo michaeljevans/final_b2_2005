78208a7e5c268b7663cc6022a7d6a976990c9c28
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 10:43:23 -0600

    Add test for displaying count of minors and adults on the flight

--- spec/features/flights/show_spec.rb
+++ spec/features/flights/show_spec.rb
@@ -22,4 +22,24 @@ RSpec.describe 'Flight show page' do
       expect(page).to_not have_content(joe.name)
     end
   end
+
+  it 'displays the number of minors and adults on the flight' do
+    frontier = Airline.create!(name: 'Frontier')
+    flight_1 = Flight.create!(number: 1727, date: '2020/08/03', time: "3:30 p.m. MST", departure_city: 'Denver', arrival_city: 'Reno', airline_id: frontier.id)
+    mike = Passenger.create!(name: 'Mike', age: 29)
+    joe = Passenger.create!(name: 'Joe', age: 7)
+    sally = Passenger.create!(name: 'Sally', age: 56)
+    sarah = Passenger.create!(name: 'Sarah', age: 4)
+    jim = Passenger.create!(name: 'Jim', age: 61)
+    FlightPassenger.create!(flight_id: flight_1.id, passenger_id: mike.id)
+    FlightPassenger.create!(flight_id: flight_1.id, passenger_id: joe.id)
+    FlightPassenger.create!(flight_id: flight_1.id, passenger_id: sally.id)
+    FlightPassenger.create!(flight_id: flight_1.id, passenger_id: sarah.id)
+    FlightPassenger.create!(flight_id: flight_1.id, passenger_id: jim.id)
+
+    visit "/flights/#{flight_1.id}"
+
+    expect(page).to have_content('Number of Minors on Flight: 2')
+    expect(page).to have_content('Number of Adults on Flight: 3')
+  end
 end
