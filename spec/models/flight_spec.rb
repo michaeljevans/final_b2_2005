bf03baec6640a2b61bb0939406ca6538b9b15c0f
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 10:42:28 -0600

    Add tests for #minors_on_board and #adults_on_board

--- spec/models/flight_spec.rb
+++ spec/models/flight_spec.rb
@@ -14,4 +14,34 @@ RSpec.describe Flight do
     it { should validate_presence_of :departure_city }
     it { should validate_presence_of :arrival_city }
   end
+
+  describe 'instance methods' do
+    it '#minors_on_board' do
+      frontier = Airline.create!(name: 'Frontier')
+      flight_1 = Flight.create!(number: 1727, date: '2020/08/03', time: "3:30 p.m. MST", departure_city: 'Denver', arrival_city: 'Reno', airline_id: frontier.id)
+      mike = Passenger.create!(name: 'Mike', age: 29)
+      joe = Passenger.create!(name: 'Joe', age: 7)
+      sarah = Passenger.create!(name: 'Sarah', age: 4)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: mike.id)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: joe.id)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: sarah.id)
+
+      expect(flight_1.minors_on_board).to eq(2)
+    end
+
+    it '#adults_on_board' do
+      frontier = Airline.create!(name: 'Frontier')
+      flight_1 = Flight.create!(number: 1727, date: '2020/08/03', time: "3:30 p.m. MST", departure_city: 'Denver', arrival_city: 'Reno', airline_id: frontier.id)
+      mike = Passenger.create!(name: 'Mike', age: 29)
+      joe = Passenger.create!(name: 'Joe', age: 7)
+      sally = Passenger.create!(name: 'Sally', age: 56)
+      jim = Passenger.create!(name: 'Jim', age: 61)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: mike.id)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: joe.id)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: sally.id)
+      FlightPassenger.create!(flight_id: flight_1.id, passenger_id: jim.id)
+
+      expect(flight_1.adults_on_board).to eq(3)
+    end
+  end
 end
