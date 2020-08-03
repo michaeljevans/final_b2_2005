b351c441f99cb7e22d4c1609afacb76f5f7db217
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 10:43:45 -0600

    Create #minors_on_board and #adults_on_board methods

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -5,4 +5,12 @@ class Flight < ApplicationRecord
   belongs_to :airline
   has_many :flight_passengers
   has_many :passengers, through: :flight_passengers
+
+  def minors_on_board
+    passengers.where('age <= ?', 18).count
+  end
+
+  def adults_on_board
+    passengers.where('age >= ?', 18).count
+  end
 end
