cb0d7313de2cf72b14364bc109f98ce7b4c50706
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 09:34:09 -0600

    Add relationships

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,5 +1,8 @@
 class Flight < ApplicationRecord
-  validates_presence_of :number
+  validates_numericality_of :number, only_integer: true
+  validates_presence_of :date, :time, :departure_city, :arrival_city
 
   belongs_to :airline
+  has_many :flight_passengers
+  has_many :passengers, through: :flight_passengers
 end
