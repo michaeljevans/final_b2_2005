60005c540db7c52da50539ac653f61b0f85ac3d8
Michael Evans <michaeljevans113@gmail.com>
2020-08-03 10:27:56 -0600

    Add post route for assigning passenger to a flight

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get '/flights/:id', to: 'flights#show'
 
   get '/passengers/:id', to: 'passengers#show'
+
+  post '/flight_passengers', to: 'flight_passengers#create'
 end
